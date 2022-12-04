17b8737d86a36d12dc5fd920c28f26bc10c50790
stanley <stanleykeyi19@gmail.com>
2022-12-04 14:06:38 +0100

    worked on settings

--- new_meetups/settings.py
+++ new_meetups/settings.py
@@ -130,9 +130,10 @@ STATICFILES_DIR=[
 ]
 
 CORS_ALLOWED_ORIGINS=[
-    "http://locahost:8000"
+    "http://locahost:8000",
+    "https://meetups-production.up.railway.app"
 ]
-
+CSRF_TRUSTED_ORIGIN=["https://meetups-production.up.railway.app"]
 CORS_ALLOW_CREDENTIALS=True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
